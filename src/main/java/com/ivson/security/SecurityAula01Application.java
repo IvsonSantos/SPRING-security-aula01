0eb59efad9c2b276ee169f94703bc64156c60745
Ivson Santos <santosivson@hotmail.com>
2018-04-14 17:54:01 -0300

    Adicionado a validacao e o bootstrap

---
+++ src/main/java/com/ivson/security/SecurityAula01Application.java
@@ -0,0 +1,12 @@
+package com.ivson.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityAula01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityAula01Application.class, args);
+	}
+}
